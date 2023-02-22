c54028b6fd4ec20e6f0b865d38576a86207ffdfb
Julian22222 <julik.golovenj@gmail.com>
2023-02-22 16:22:03 +0000

    GitHub pages

--- src/main.js
+++ src/main.js
@@ -1,60 +1,60 @@
-// // console.log("Hello")
+// console.log("Hello")
 
-// // const colors = [
-// //   "#FF5733",
-// //   "#F9FF33",
-// //   "#3371FF",
-// //   "red",
-// //   "green",
-// //   "rgb(85,255,51)",
-// // ];
-
-// const hex = [
-//   "0",
-//   "1",
-//   "2",
-//   "3",
-//   "4",
-//   "5",
-//   "6",
-//   "7",
-//   "8",
-//   "9",
-//   "A",
-//   "B",
-//   "C",
-//   "D",
-//   "E",
-//   "F",
+// const colors = [
+//   "#FF5733",
+//   "#F9FF33",
+//   "#3371FF",
+//   "red",
+//   "green",
+//   "rgb(85,255,51)",
 // ];
 
-// // document allow to interact with DOM tree
-// // document.getElementById("btn") -allow to catch the button with ID -> btn
-// // and allow to interact with that button
-// const button = document.getElementById("btn");
-// // console.log(button);
+const hex = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
 
-// const color = document.querySelector(".color");
+// document allow to interact with DOM tree
+// document.getElementById("btn") -allow to catch the button with ID -> btn
+// and allow to interact with that button
+const button = document.getElementById("btn");
+// console.log(button);
 
-// button.addEventListener("click", () => {
-//   let hexColor = generateHex();
-//   // access body backgroundColor
-//   document.body.style.backgroundColor = hexColor;
+const color = document.querySelector(".color");
 
-//   //   textContent - allow as to put new text
-//   color.textContent = hexColor;
-// });
+button.addEventListener("click", () => {
+  let hexColor = generateHex();
+  // access body backgroundColor
+  document.body.style.backgroundColor = hexColor;
 
-// function generateHex() {
-//   // return random code conating # + random hex elements(6 times)
-//   let hexColor = "#";
-//   for (let i = 0; i < 6; i++) {
-//     hexColor += hex[getRandomNumber()];
-//   }
-//   return hexColor;
-// }
+  //   textContent - allow as to put new text
+  color.textContent = hexColor;
+});
 
-// function getRandomNumber() {
-//   // gives a random number from 0 - 15
-//   return Math.floor(Math.random() * hex.length);
-// }
+function generateHex() {
+  // return random code conating # + random hex elements(6 times)
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  return hexColor;
+}
+
+function getRandomNumber() {
+  // gives a random number from 0 - 15
+  return Math.floor(Math.random() * hex.length);
+}
